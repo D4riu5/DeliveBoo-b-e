af26095d52efef0291102e5da8f46114483e4e34
Antek93 <antonio.savino1993@gmail.com>
2023-04-12 16:35:21 +0200

    aggiunto index e show di type

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Type;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,8 @@ class TypeController extends Controller
      */
     public function index()
     {
-        //
+        $types = Type::all();
+        return view('admin.type.index', compact('types'));
     }
 
     /**
@@ -46,7 +48,7 @@ class TypeController extends Controller
      */
     public function show(Type $type)
     {
-        //
+        return view('admin.type.show', compact('type'));
     }
 
     /**
---
+++ resources/views/admin/type/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <h1>
+                    Cucina: {{ $type->name }}
+                </h1>
+                <p>
+                    
+                </p>
+            </div>
+        </div>
+    </div>
+@endsection
