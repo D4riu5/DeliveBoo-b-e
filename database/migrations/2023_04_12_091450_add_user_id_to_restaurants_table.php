736334740bd1072c6bd86e22a5793126483f68a0
D4riu5 <R3iji94@gmail.com>
2023-04-12 12:14:43 +0200

    fixed on delete user error

--- database/migrations/2023_04_12_091450_add_user_id_to_restaurants_table.php
+++ database/migrations/2023_04_12_091450_add_user_id_to_restaurants_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('restaurants', function (Blueprint $table) {
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('user_id')->after('id');
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
