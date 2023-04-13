2e3d824b82f97948e0cab82933c7f29735313ffd
Antek93 <antonio.savino1993@gmail.com>
2023-04-13 18:04:29 +0200

    immagini cliccabili

--- resources/views/admin/type/index.blade.php
+++ resources/views/admin/type/index.blade.php
@@ -10,18 +10,21 @@
                     </h2>
                 </div>
                 {{-- INIZIO CICLO PER STAMPA DI ELEMENTI IN VARIABILE $TYPES  --}}
-                <div class="btn-group my-2 d-flex justify-content-evenly flex-wrap" role="group" aria-label="Basic mixed styles example">
+                <div class="btn-group my-2 d-flex justify-content-evenly flex-wrap" role="group"
+                    aria-label="Basic mixed styles example">
                     @foreach ($types as $type)
-                    <div class="d-flex flex-column align-items-center mb-3">
-                        <div class="py-1">
-                            <a class="btn btn-success mx-1" href="{{ route('admin.type.show', $type->id) }}">
-                                {{ $type->name }}
-                            </a>
+                        <div class="d-flex flex-column align-items-center mb-3">
+                            <div class="py-1">
+                                <a class="btn btn-success mx-1" href="{{ route('admin.type.show', $type->id) }}">
+                                    {{ $type->name }}
+                                </a>
+                            </div>
+                            <div class="typePhotoBox">
+                                <a href="{{ route('admin.type.show', $type->id) }}">
+                                    <img src="{{ asset('storage/' . $type->image) }}">
+                                </a>
+                            </div>
                         </div>
-                        <div class="typePhotoBox mx-3">
-                            <img src="{{ asset("storage/" . $type->image) }}">
-                        </div>
-                    </div>
                     @endforeach
                     {{-- FINE CICLO PER STAMPA DI ELEMENTI IN VARIABILE $TYPES  --}}
                 </div>
