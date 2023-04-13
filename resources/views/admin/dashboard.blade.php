9643da6133eaf823cc6fe805b64f115d491588ff
Antek93 <antonio.savino1993@gmail.com>
2023-04-13 17:23:06 +0200

    per immagini

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -60,7 +60,7 @@
                             </td>
                             <td>
                                 <a class="text-decoration-none " href="#">
-                                        Vai al mio Menù
+                                    Vai al mio Menù
                                 </a>
                             </td>
                         </tr>
@@ -71,28 +71,26 @@
         {{-- NOME ATTIVITA' + TIPOLOGIE + FOTO ATTIVITA' --}}
         <div class="row justify-content-evenly">
             <div>
-                <div class="my-3">
-                    <h2 class="text-center mt-5">
-                        {{ $user->restaurant->name }}
-                    </h2>
-                    <div class="text-center">
-                        <p>
-                            E' un attività che rientra nelle seguenti tipologie di cucina:
-                        </p>
-                        @foreach ($user->restaurant->types as $type)
-                            <a class="m-2 d-inline-block" href="{{ route('admin.type.show', $type->id) }}">
-                                <h4 class="btn btn-warning">
-                                    {{ $type->name }}
-                                </h4>
-                            </a>
-                        @endforeach
-                        <hr>
-                    </div>
-                    <div>
-                        <h1 class="text-center">
-                            Foto attività
-                        </h1>
-                    </div>
+                <h2 class="text-center mt-3">
+                    {{ $user->restaurant->name }}
+                </h2>
+                <div class="text-center">
+                    <p>
+                        E' un attività che rientra nelle seguenti tipologie di cucina:
+                    </p>
+                    @foreach ($user->restaurant->types as $type)
+                        <a class="m-2 d-inline-block" href="{{ route('admin.type.show', $type->id) }}">
+                            <h4 class="btn btn-warning">
+                                {{ $type->name }}
+                            </h4>
+                        </a>
+                    @endforeach
+                    <hr>
+                </div>
+                <div>
+                    <h1 class="text-center">
+                        Foto attività
+                    </h1>
                 </div>
             </div>
         </div>
--- resources/views/admin/type/index.blade.php
+++ resources/views/admin/type/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-fluid mt-4">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-12">
                 <div class="mb-5">
                     <h2 class="fw-bold text-center mt-2">
@@ -10,9 +10,9 @@
                     </h2>
                 </div>
                 {{-- INIZIO CICLO PER STAMPA DI ELEMENTI IN VARIABILE $TYPES  --}}
-                <div class="btn-group my-2" role="group" aria-label="Basic mixed styles example">
+                <div class="btn-group my-2 d-flex justify-content-evenly flex-wrap" role="group" aria-label="Basic mixed styles example">
                     @foreach ($types as $type)
-                    <div class="d-flex flex-column align-items-center">
+                    <div class="d-flex flex-column align-items-center mb-3">
                         <div class="py-1">
                             <a class="btn btn-success mx-1" href="{{ route('admin.type.show', $type->id) }}">
                                 {{ $type->name }}
