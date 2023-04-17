1455669fb431f8d3ddefc1db574eb7ada86056af
D4riu5 <R3iji94@gmail.com>
2023-04-17 22:08:40 +0200

    Added Types API controller + index and show routes

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -15,6 +15,24 @@ class Type extends Model
         'image'
     ];
 
+    // appends the return of getFullImgPathAttribute to the Project istance (use name from function get FullImagePath Attribute and conv to snake case)
+    protected $appends = ['full_image_path'];
+
+    // Hidden ONLY from query (hiding image so only full_image_path is visible from query)
+    protected $hidden = [
+        'image',
+    ];
+
+    // function to append image storage to use in front end with API
+    public function getFullImagePathAttribute() {
+        $fullPath = null;
+        if ($this->image) {
+            $fullPath = asset('storage/'.$this->image);
+        }
+
+        return $fullPath;
+    }   
+
     public function restaurants() {
         return $this->belongsToMany(Restaurant::class);
     }
