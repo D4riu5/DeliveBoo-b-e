30bab6d49e84cb3853040383da4e24df3aed2ced
Antek93 <antonio.savino1993@gmail.com>
2023-04-24 16:52:35 +0200

    320 px resonsiver fix

--- resources/views/admin/food/index.blade.php
+++ resources/views/admin/food/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container-fluid mt-4">
-        <div class="row justify-content-center mb-4">
+        <div class="row justify-content-center mb-2">
             <div class="col text-center">
                 <h1>
                     I miei piatti
@@ -12,10 +12,42 @@
 
         @include('partials.success')
 
-        <div class="row my-4 d-flex">
-            <div class="col d-flex justify-content-center">
-                <form action="{{ route('admin.food.index') }}" method="GET">
-                    <div class="d-flex justify-content-center">
+        <div class="bigScreen">
+            <div class="row my-4 d-flex">
+                <div class="col d-flex justify-content-center">
+                    <form action="{{ route('admin.food.index') }}" method="GET">
+                        <div class="d-flex justify-content-center">
+                            <div class="d-flex align-items-center">
+                                <div class="mx-1">
+                                    <button type="submit" class="btn btn-danger my-2">
+                                        Cerca
+                                    </button>
+                                </div>
+                                <div>
+                                    <input type="text" name="name"
+                                        placeholder="Cerca piatto per nome..               " class="form-control "
+                                        value="{{ request()->input('name') }}">
+                                </div>
+                            </div>
+                            <div class="mx-1">
+                                <a href="{{ route('admin.food.create') }}" class="btn btn-warning mt-2">
+                                    Aggiungi un piatto
+                                </a>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="smallScreen ">
+            <div class="mx-1 d-flex justify-content-center">
+                <a href="{{ route('admin.food.create') }}" class="btn btn-warning mt-2">
+                    Aggiungi un piatto
+                </a>
+            </div>
+            <div class="row my-4 d-flex">
+                <div class="col d-flex justify-content-center">
+                    <form action="{{ route('admin.food.index') }}" method="GET">
                         <div class="d-flex align-items-center">
                             <div class="mx-1">
                                 <button type="submit" class="btn btn-danger my-2">
@@ -27,16 +59,12 @@
                                     class="form-control " value="{{ request()->input('name') }}">
                             </div>
                         </div>
-                        <div class="mx-1">
-                            <a href="{{ route('admin.food.create') }}" class="btn btn-warning mt-2">
-                                Aggiungi un piatto
-                            </a>
-                        </div>
-                    </div>
+                </div>
                 </form>
             </div>
         </div>
-
+    </div>
+    <div class="bigScreen">
         <div class="row">
             <div class="col">
                 <table class="table">
@@ -122,9 +150,102 @@
 
                 {{-- PAGINATION (FIX STYLE) --}}
                 {{-- <div class="pagination-container my-3">
-                    {{ $food->links() }}
-                </div> --}}
+                        {{ $food->links() }}
+                    </div> --}}
             </div>
         </div>
+    </div>
+    <div class="smallScreen">
+        <div class="row">
+            <div class="col">
+                <table class="table">
+                    @foreach ($foods as $food)
+                        <div class="card card-title">
+                            <div class="d-flex flex-column align-items-center">
+                                <a style="color:green" href="{{ route('admin.food.show', $food->id) }}">
+                                    {{ $food->name }}
+                                </a>
+                            </div>
+                            <div class="d-flex mt-2">
+                                <span>Prezzo: </span>
+                                <span class="ms-1">{{ $food->price }}€</span>
+                            </div>
+                            <div class="d-flex mt-2">
+                                <span>Portata:</span>
+                                <span class="ms-1">{{ $food->course }}</span>
+                            </div>
+                            <div class="d-flex mt-2">
+                                <span>Disponibile:</span>
+                                <span class="ms-1">{{ $food->is_available ? 'Si' : 'No' }}</span>
+                            </div>
+                            {{-- DISPOSIZIONE BOTTONI  --}}
+                            <div class="d-flex justify-content-center my-2">
+                                {{-- BOTTOEN VERDE PER INFO --}}
+                                <div class="mx-1">
+                                    <a href="{{ route('admin.food.show', $food->id) }}" class="btn btn-success">
+                                        <i class="fa-solid fa-circle-info"></i>
+                                    </a>
+                                </div>
+                                {{-- BOTTONE GIALLO PER MODIFICA --}}
+                                <div class="mx-3">
+                                    <a href="{{ route('admin.food.edit', $food->id) }}" class="btn btn-warning">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
+                                </div>
+                                {{-- BOTTONE ROSSO PER ELIMINA --}}
+                                <div>
+                                    <form class="d-inline-block" action="{{ route('admin.food.destroy', $food->id) }}"
+                                        method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger" type="button" data-bs-toggle="modal"
+                                            data-bs-target="#exampleModal{{ $food->id }}">
+                                            <i class="fa-solid fa-trash"></i>
+                                        </button>
+
+                                        <div class="modal fade" id="exampleModal{{ $food->id }}" tabindex="-1"
+                                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+                                            <div class="modal-dialog">
+
+                                                <div class="modal-content">
+
+                                                    <div class="modal-header">
+                                                        <h1 class="modal-title fs-5" id="exampleModalLabel"> Conferma
+                                                            eliminazione
+                                                        </h1>
+                                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                            aria-label="Close"></button>
+                                                    </div>
+
+                                                    <div class="modal-body">
+                                                        Sei sicuro di voler eliminare questo piatto ?
+                                                    </div>
+
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-bs-dismiss="modal">
+                                                            Annulla </button>
+                                                        <button type="submit" class="btn btn-danger"> Elimina </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+            </div>
+            @endforeach
+            {{-- PAGINATION (FIX STYLE) --}}
+            {{-- <div class="pagination-container my-3">
+                        {{ $food->links() }}
+                    </div> --}}
+        </div>
+    </div>
+    </div>
     </div>
 @endsection
--- resources/views/admin/food/show.blade.php
+++ resources/views/admin/food/show.blade.php
@@ -5,140 +5,278 @@
         <div class="row justify-content-center mb-4">
             <div class="col">
                 @include('partials.success')
+                <div class="bigScreen">
+                    <h1>
+                        {{ $food->name }}
+                    </h1>
+                    <div class="d-flex">
+                        {{-- IMMAGINE --}}
+                        <div class="col-auto">
+                            @if ($food->image != null)
+                                <div class="photoBox">
+                                    <img src="{{ asset('storage/' . $food->image) }}" alt="food">
+                                </div>
+                            @else
+                                <div class="placeholderBox">
+                                    <img src="{{ asset('storage/placeholder/placeholder.jpg') }}" style="height: 400px;"
+                                        alt="placeholder">
+                                </div>
+                            @endif
+                        </div>
+                        {{-- Descrizione prodotto  --}}
+                        <div class="col-3 px-1 mt-1">
+                            <h3 class="my-2">
+                                Descrizione:
+                            </h3>
+                            <p>
+                                {!! nl2br($food->description) !!}
+                            </p>
+                        </div>
+                    </div>
 
-                <h1 >
-                    {{ $food->name }}
-                </h1>
-                <div class="d-flex">
-                    {{-- IMMAGINE --}}
-                    <div class="col-auto">
-                        @if ($food->image != null)
-                            <div class="photoBox">
-                                <img src="{{ asset('storage/' . $food->image) }}"
-                                    alt="food">
+                    {{-- CONTENITORE INFO PER LO SHOW - FLEXATO --}}
+                    <div class="d-flex">
+                        {{-- BLOCCO INFO A SINISTRA --}}
+                        <div>
+                            {{-- Dettaglio Portata --}}
+                            <div>
+                                <h5 class="my-2">
+                                    Portata:
+                                </h5>
+                                <p>
+                                    {{ $food->course }}
+                                </p>
                             </div>
-                        @else
-                            <div class="placeholderBox">
-                                <img src="{{ asset('storage/placeholder/placeholder.jpg') }}"
-                                    style="height: 400px;" alt="placeholder">
+                            {{-- Status disponibilità --}}
+                            <div>
+                                <h5 class="my-2">
+                                    Disponibile?
+                                </h5>
+                                <p>
+                                    {{ $food->is_available ? 'Si' : 'No' }}
+                                </p>
                             </div>
-                        @endif
+                            {{-- Prezzo prodotto  --}}
+                            <div>
+                                <h5 class="my-2">
+                                    Prezzo:
+                                </h5>
+                                <p>
+                                    {{ $food->price }} €
+                                </p>
+                            </div>
+                        </div>
+
+                        {{-- BLOCCO INFO A SINISTRA --}}
+                        <div class="rightBox pt-1">
+
+
+                            {{-- INFO PICCANTE  --}}
+                            <div>
+                                <h5 class="my-2">
+                                    E' un prodotto piccante?
+                                </h5>
+                                @if ($food->food_detail->spicy !== null)
+                                    @if ($food->food_detail->spicy == 1)
+                                        <p>Si</p>
+                                    @elseif ($food->food_detail->spicy == 0)
+                                        <p>No</p>
+                                    @endif
+                                @else
+                                    <p>Non specificato</p>
+                                @endif
+                            </div>
+
+
+
+                            {{-- INFO GLUTINE  --}}
+                            <div>
+                                <h5 class="my-2">
+                                    Contiene Glutine?
+                                </h5>
+                                @if ($food->food_detail->gluten_free !== null)
+                                    @if ($food->food_detail->gluten_free == 1)
+                                        <p>Si</p>
+                                    @elseif ($food->food_detail->gluten_free == 0)
+                                        <p>No</p>
+                                    @endif
+                                @else
+                                    <p>Non specificato</p>
+                                @endif
+                            </div>
+
+                            {{-- CALORIE CONTENUTE  --}}
+                            <div>
+                                <h5 class="my-2">
+                                    Quante calorie contiene?
+                                </h5>
+                                @if ($food->food_detail && $food->food_detail->kcal !== null)
+                                    {{ $food->food_detail->kcal . ' kcal' }}
+                                @else
+                                    <p>
+                                        Non specificato
+                                    </p>
+                                @endif
+                            </div>
+
+                        </div>
+                    </div>
+                    {{-- PULSANTE RITORNO A MIEI PRODOTTI  --}}
+                    <div class="">
+                        <a class="mt-2 btn btn-danger"
+                            class="{{ Route::currentRouteName() == 'admin.food.index' ? 'bg-secondary' : '' }}"
+                            href="{{ route('admin.food.index') }}">
+                            <h6>
+                                Torna sul mio menù
+                            </h6>
+                        </a>
+                        <a href="{{ route('admin.food.edit', $food->id) }}" class="mt-2 btn btn-warning">
+                            <h6>
+                                Modifica piatto
+                            </h6>
+                        </a>
+                    </div>
+
+                </div>
+                <div class="smallScreen">
+                    <h3 class="text-center">
+                        {{ $food->name }}
+                    </h3>
+                    <div class="d-flex ">
+                        {{-- IMMAGINE --}}
+                        <div class="col-12">
+                            @if ($food->image != null)
+                                <div class="photoBox">
+                                    <img src="{{ asset('storage/' . $food->image) }}" alt="food">
+                                </div>
+                            @else
+                                <div class="placeholderBox">
+                                    <img src="{{ asset('storage/placeholder/placeholder.jpg') }}" alt="placeholder">
+                                </div>
+                            @endif
+                        </div>
                     </div>
                     {{-- Descrizione prodotto  --}}
-                    <div class="col-3 px-1 mt-1">
-                        <h3 class="my-2">
+                    <div class="col-12">
+                        <h4>
                             Descrizione:
-                        </h3>
+                        </h4>
                         <p>
                             {!! nl2br($food->description) !!}
                         </p>
                     </div>
-                </div>
-
-                {{-- CONTENITORE INFO PER LO SHOW - FLEXATO --}}
-                <div class="d-flex">
-                    {{-- BLOCCO INFO A SINISTRA --}}
-                    <div>
-                        {{-- Dettaglio Portata --}}
-                        <div>
-                            <h5 class="my-2">
-                                Portata:
-                            </h5>
-                            <p>
-                                {{ $food->course }}
-                            </p>
-                        </div>
-                        {{-- Status disponibilità --}}
-                        <div>
-                            <h5 class="my-2">
-                                Disponibile?
-                            </h5>
-                            <p>
-                                {{ $food->is_available ? 'Si' : 'No' }}
-                            </p>
-                        </div>
-                        {{-- Prezzo prodotto  --}}
-                        <div>
-                            <h5 class="my-2">
-                                Prezzo:
-                            </h5>
-                            <p>
-                                {{ $food->price }} €
-                            </p>
-                        </div>
+                    <div class="col-12">
+                        <h4 class="my-1">
+                            Portata:
+                        </h4>
+                        <p>
+                            {{ $food->course }}
+                        </p>
                     </div>
-
-                    {{-- BLOCCO INFO A SINISTRA --}}
-                    <div class="rightBox pt-1">
-
-
-                        {{-- INFO PICCANTE  --}}
-                        <div>
-                            <h5 class="my-2">
-                                E' un prodotto piccante?
-                            </h5>
-                            @if ($food->food_detail->spicy !== null)
-                                @if ($food->food_detail->spicy == 1)
-                                    <p>Si</p>
-                                @elseif ($food->food_detail->spicy == 0)
-                                    <p>No</p>
-                                @endif
-                            @else
-                                <p>Non specificato</p>
-                            @endif
-                        </div>
-
-
-
-                        {{-- INFO GLUTINE  --}}
-                        <div>
-                            <h5 class="my-2">
-                                Contiene Glutine?
-                            </h5>
-                            @if ($food->food_detail->gluten_free !== null)
-                                @if ($food->food_detail->gluten_free == 1)
-                                    <p>Si</p>
-                                @elseif ($food->food_detail->gluten_free == 0)
-                                    <p>No</p>
-                                @endif
-                            @else
-                                <p>Non specificato</p>
-                            @endif
-                        </div>
-
-                        {{-- CALORIE CONTENUTE  --}}
-                        <div>
-                            <h5 class="my-2">
-                                Quante calorie contiene?
-                            </h5>
-                            @if ($food->food_detail && $food->food_detail->kcal !== null)
-                                {{ $food->food_detail->kcal . ' kcal' }}
-                            @else
-                                <p>
-                                    Non specificato
-                                </p>
-                            @endif
-                        </div>
-
+                    <div class="col-12">
+                        <h4 class="my-1">
+                            Disponibile?:
+                        </h4>
+                        <p>
+                            {{ $food->is_available ? 'Si' : 'No' }}
+                        </p>
+                    </div>
+                    <div class="col-12">
+                        <h4 class="my-1">
+                            Prezzo:
+                        </h4>
+                        <p>
+                            {{ $food->price }} €
+                        </p>
                     </div>
                 </div>
-                {{-- PULSANTE RITORNO A MIEI PRODOTTI  --}}
-                <div class="">
-                    <a class="mt-2 btn btn-danger"
-                        class="{{ Route::currentRouteName() == 'admin.food.index' ? 'bg-secondary' : '' }}"
-                        href="{{ route('admin.food.index') }}">
-                        <h6>
-                            Torna sul mio menù
-                        </h6>
-                    </a>
-                    <a href="{{ route('admin.food.edit', $food->id) }}" class="mt-2 btn btn-warning">
-                        <h6>
-                            Modifica piatto
-                        </h6>
-                    </a>
-                </div>
 
+
+                {{-- Status disponibilità --}}
+                <div>
+                    <h5 class="my-2">
+                        Disponibile?
+                    </h5>
+                    <p>
+                        {{ $food->is_available ? 'Si' : 'No' }}
+                    </p>
+                </div>
+                {{-- Prezzo prodotto  --}}
+                <div>
+                    <h5 class="my-2">
+                        Prezzo:
+                    </h5>
+                    <p>
+                        {{ $food->price }} €
+                    </p>
+                </div>
+                <div>
+                    <h5 class="my-2">
+                        E' un prodotto piccante?
+                    </h5>
+                    <p>
+                        @if ($food->food_detail->spicy !== null)
+                            @if ($food->food_detail->spicy == 1)
+                                <p>Si</p>
+                            @elseif ($food->food_detail->spicy == 0)
+                                <p>No</p>
+                            @endif
+                        @else
+                            <p>Non specificato</p>
+                        @endif
+                    </p>
+                </div>
+                <div>
+                    <h5 class="my-2">
+                        Contiene glutine?
+                    </h5>
+                    <p>
+                        @if ($food->food_detail->gluten_free !== null)
+                        @if ($food->food_detail->gluten_free == 1)
+                            <p>Si</p>
+                        @elseif ($food->food_detail->gluten_free == 0)
+                            <p>No</p>
+                        @endif
+                    @else
+                        <p>Non specificato</p>
+                    @endif
+                    </p>
+                </div>
+                <div>
+                    <h5 class="my-2">
+                        Quante calorie contiene?:
+                    </h5>
+                    <p>
+                        @if ($food->food_detail && $food->food_detail->kcal !== null)
+                        {{ $food->food_detail->kcal . ' kcal' }}
+                    @else
+                        <p>
+                            Non specificato
+                        </p>
+                    @endif
+                    </p>
+                </div>
+            </div>
             </div>
         </div>
+        {{-- PULSANTE RITORNO A MIEI PRODOTTI  --}}
+        <div class="d-flex justify-content-center">
+            <a class="mt-2 btn btn-danger"
+                class="{{ Route::currentRouteName() == 'admin.food.index' ? 'bg-secondary' : '' }}"
+                href="{{ route('admin.food.index') }}">
+                <h6>
+                    Torna sul mio menù
+                </h6>
+            </a>
+            <a href="{{ route('admin.food.edit', $food->id) }}" class="mt-2 btn btn-warning">
+                <h6>
+                    Modifica piatto
+                </h6>
+            </a>
+        </div>
+
+    </div>
+    </div>
+    </div>
     </div>
 @endsection
