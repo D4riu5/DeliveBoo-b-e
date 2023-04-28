8ae8f1c4850c0d898c75b5d9c3f9b6d51711e5dc
Antek93 <antonio.savino1993@gmail.com>
2023-04-28 15:13:59 +0200

    1024px responsive

--- resources/views/admin/food/create.blade.php
+++ resources/views/admin/food/create.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container-fluid mt-4">
+    <div class="container mt-4">
         <div class="row justify-content-center mb-4">
             <div class="col">
                 <h1>
